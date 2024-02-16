d669498ef159e28ea986538d3eee264798f1ce7d
Shaik <shaikadam642@gmail.com>
2024-02-16 20:04:28 +0530

    first commit

---
+++ public/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const path = require("path")
+const { Server } = require("socket.io");
+const app = express();
+const port = 8000;
+const server = http.createServer(app);
+const io = new Server(server)
+
+//socket io
+io.on("connection",(socket) => {
+    socket.on("user-message",(message) => {
+        io.emit('message',message)
+        console.log(message)
+    })
+})
+app.get("/",(req,res) => {
+    res.sendFile(__dirname+"/index.html")
+})
+
+server.listen(port,() => {
+    console.log("Listnening on port 8000")
+})
