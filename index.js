02c340cce39ffcf1006a1c65d1225e6650e952b3
Shaik <shaikadam642@gmail.com>
2024-02-16 23:58:04 +0530

    audio update

--- index.js
+++ index.js
@@ -8,25 +8,40 @@ const server = http.createServer(app);
 const io = new Server(server)
 
 //socket io
-io.on("connection",(socket) => {
-    console.log(`user connected: ${socket.id}`)
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
 
-    socket.on("join-room",(room) => {
+    socket.on("join-room", (room) => {
         socket.join(room);
-        console.log(`User ${socket.id} joined room ${room}`)
-    })
-
-
-    socket.on("user-message",(data) => {
-        const {room,message} = data;
-        io.to(room).emit('message',message)
-        console.log(`message from ${socket.id} in room ${room}: ${message}`)
+        console.log(`User ${socket.id} joined room ${room}`);
     });
 
-    socket.on("disconnect",() => {
+    // Handle audio stream
+    socket.on("audio-stream", (stream) => {
+        // Broadcast the audio stream to other clients in the same room
+        socket.to(room).broadcast.emit("audio-stream", stream);
+    });
+
+    // Handle text and audio messages
+    socket.on("user-message", (data) => {
+        const { room, message } = data;
+        io.to(room).emit('message', message);
+        console.log(`Message from ${socket.id} in room ${room}: ${message}`);
+    });
+
+    socket.on("audio-message", (data) => {
+        const { room, audioData } = data;
+        // Do something with the audio data, such as broadcasting it to other clients
+        io.to(room).emit('audio-message', audioData);
+        console.log(`Audio message from ${socket.id} in room ${room}`);
+    });
+
+    // Handle disconnection
+    socket.on("disconnect", () => {
         console.log(`User disconnected: ${socket.id}`);
-    })
-})
+    });
+});
+
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 })
