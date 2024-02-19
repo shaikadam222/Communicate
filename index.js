09139e362947775f45e7f69f685b9c0f29c98a23
Shaik <shaikadam642@gmail.com>
2024-02-19 11:42:39 +0530

    no idea

--- index.js
+++ index.js
@@ -2,27 +2,17 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const { Server } = require("socket.io");
+
 const app = express();
 const port = 8000;
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Global object to store rooms and their users
-const rooms = {};
-
 io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
 
-    socket.on("join-room", (data) => {
-        const { room } = data;
+    socket.on("join-room", (room) => {
         socket.join(room);
-
-        // Add the user to the room's user list
-        if (!rooms[room]) {
-            rooms[room] = [];
-        }
-        rooms[room].push(socket.id);
-
         console.log(`User ${socket.id} joined room ${room}`);
     });
 
@@ -32,37 +22,8 @@ io.on("connection", (socket) => {
         console.log(`Message from ${socket.id} in room ${room}: ${message}`);
     });
 
-    // Handle audio stream
-    socket.on("audio-stream", (data) => {
-        const { room, stream } = data;
-
-        // Broadcast the audio stream to other clients in the same room
-        socket.to(room).broadcast.emit("audio-stream", stream);
-
-        console.log(`Audio stream from ${socket.id} in room ${room}`);
-    });
-
-    // Handle audio messages
-    socket.on("audio-message", (data) => {
-        const { room, audioData } = data;
-
-        // Broadcast the audio message to other clients in the same room
-        socket.to(room).broadcast.emit("audio-message", audioData);
-
-        console.log(`Audio message from ${socket.id} in room ${room}`);
-    });
-
     socket.on("disconnect", () => {
         console.log(`User disconnected: ${socket.id}`);
-
-        // Remove the user from the room's user list
-        for (const room in rooms) {
-            const index = rooms[room].indexOf(socket.id);
-            if (index !== -1) {
-                rooms[room].splice(index, 1);
-                break;
-            }
-        }
     });
 });
 
